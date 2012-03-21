ce220ab14611995027da3bdebc5cc6eb0b7b2f35
David L <leblancd@gmail.com>
2012-03-21 10:46:45 -0400

    Removed main method (no longer necessary with Python 2.7)

--- GAE/clikcare/clikcare.py
+++ GAE/clikcare/clikcare.py
@@ -78,10 +78,3 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                       ('/provider/schedule', ProviderSchedule),
                                       ('/provider/profile', ProviderProfile)],
                                      debug=True)
-
-
-def main():
-    run_wsgi_app(app)
-
-if __name__ == "__main__":
-    main()
