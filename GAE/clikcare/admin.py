8bfa4417432d5b84983850c6018a728ecc62e63d
phiiil <phiiil3737@gmail.com>
2012-03-23 21:15:07 -0400

    Added admin template and handler

---
+++ GAE/clikcare/admin.py
@@ -0,0 +1,15 @@
+'''
+    admin handlers
+'''
+
+from google.appengine.ext import db
+from base import BaseHandler
+
+class IndexHandler(BaseHandler):
+    def get(self):
+        
+        bookings = db.GqlQuery("SELECT * FROM Booking ORDER BY createdOn DESC LIMIT 10")
+
+        tv = { 'bookings': bookings }
+        
+        self.render_template('admin/index.html', **tv)
