8115920c18549f6f9ab14976cff1d66a14f4dae5
phiiil <phiiil3737@gmail.com>
2012-03-23 21:26:15 -0400

    removed BaseHandler introduced in merge error

--- GAE/clikcare/main.py
+++ GAE/clikcare/main.py
@@ -8,15 +8,6 @@ from base import BaseHandler
 import admin
 
 
-class BaseHandler(webapp2.RequestHandler):
-        
-    @webapp2.cached_property
-    def jinja2(self):
-        return jinja2.get_jinja2(app=self.app)
-
-    def render_template(self, filename, **template_args):
-        self.response.write(self.jinja2.render_template(filename, **template_args))
-
 # WTF! WTF doesn't come with checkboxes out of the box
 class MultiCheckboxField(SelectMultipleField):
     """
