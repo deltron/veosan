a06f3d6ca3a06e77069d033f69180629a87865df
phiiil <phiiil3737@gmail.com>
2012-03-23 21:15:57 -0400

    renamed to data (was db)

---
+++ GAE/clikcare/data.py
@@ -0,0 +1,49 @@
+'''
+Created on Mar 17, 2012
+
+@author: phil
+'''
+
+from google.appengine.ext import db
+
+
+'''
+    stored data 
+'''
+
+class Patient(db.Model):
+    '''
+    A patient
+    '''
+    createdOn = db.DateTimeProperty(auto_now_add=True)
+    firstName = db.StringProperty()
+    lastName = db.StringProperty()
+    email = db.StringProperty()
+    phone = db.StringProperty()
+
+
+class Provider(db.Model):
+    '''
+    A provider
+    '''
+    createdOn = db.DateTimeProperty(auto_now_add=True)
+    firstName = db.StringProperty()
+    lastName = db.StringProperty()
+    email = db.StringProperty()
+    phone = db.StringProperty()    
+    specialty = db.StringProperty()
+    
+    
+class Booking(db.Model):
+    '''
+    A booking
+    '''
+    createdOn = db.DateTimeProperty(auto_now_add=True)
+    #author = db.UserProperty()
+    requestSpecialty = db.StringProperty()
+    requestLocation = db.StringProperty()
+    requestDate = db.StringProperty()
+    requestTime = db.StringProperty()
+    requestContact = db.StringProperty()
+        
+        
