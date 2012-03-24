57296770d9ff931c6d2a5aaa60de8ca444fc1d14
phiiil <phiiil3737@gmail.com>
2012-03-23 21:16:19 -0400

    now db access (was data)

--- GAE/clikcare/db.py
+++ GAE/clikcare/db.py
@@ -1,46 +1,9 @@
 '''
-Created on Mar 17, 2012
-
-@author: phil
+    database access
 '''
-
-from google.appengine.ext import db
-
-
-
-class Patient(db.Model):
-    '''
-    A patient
-    '''
-    createdOn = db.DateTimeProperty(auto_now_add=True)
-    firstName = db.StringProperty()
-    lastName = db.StringProperty()
-    email = db.StringProperty()
-    phone = db.StringProperty()
-
-
-class Provider(db.Model):
-    '''
-    A provider
-    '''
-    createdOn = db.DateTimeProperty(auto_now_add=True)
-    firstName = db.StringProperty()
-    lastName = db.StringProperty()
-    email = db.StringProperty()
-    phone = db.StringProperty()    
-    specialty = db.StringProperty()
-    
-    
-class Booking(db.Model):
-    '''
-    A booking
-    '''
-    createdOn = db.DateTimeProperty(auto_now_add=True)
-    #author = db.UserProperty()
-    requestSpecialty = db.StringProperty()
-    requestLocation = db.StringProperty()
-    requestDate = db.StringProperty()
-    requestTime = db.StringProperty()
-    requestContact = db.StringProperty()
-        
-        
+            
+            
+            
+            booking = Booking()
+            booking.requestSpecialty = self.request.get('categories')
+            booking.put()
