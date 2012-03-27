46d0e3c890ec51e6ff292325fd6818cd34796cd0
phiiil <phiiil3737@gmail.com>
2012-03-26 23:32:41 -0400

    Added list of providers on dashboard

--- GAE/clikcare/admin.py
+++ GAE/clikcare/admin.py
@@ -6,10 +6,14 @@ from google.appengine.ext import db
 from base import BaseHandler
 
 class IndexHandler(BaseHandler):
+    '''
+        Admin Index
+    '''
     def get(self):
-        
         bookings = db.GqlQuery("SELECT * FROM Booking ORDER BY createdOn DESC LIMIT 10")
-
-        tv = { 'bookings': bookings }
-        
+        providers = db.GqlQuery("SELECT * from Provider ORDER BY lastName ASC LIMIT 50")
+        tv = {
+              'bookings': bookings,
+              'providers': providers 
+              }    
         self.render_template('admin/adminindex.html', **tv)
