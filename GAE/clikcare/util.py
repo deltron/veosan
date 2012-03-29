53dee4de8a562be594ac1d43732747aa9bb73af0
David L <leblancd@gmail.com>
2012-03-28 21:15:50 -0400

    added more localisation strings

--- GAE/clikcare/util.py
+++ GAE/clikcare/util.py
@@ -1,47 +1,49 @@
 # -*- coding: utf-8 -*-
+
 from datetime import date, timedelta
 import logging
 from babel.dates import format_date, format_datetime
 import gettext
 
+t = gettext.translation('clikcare', 'locale', languages=['fr'], fallback=False)
+t.install()
+
 # key, value
 def getAllRegions():
-    t = gettext.translation('clikcare', 'locale', languages=['fr'], fallback=False)
-    _ = t.ugettext
-    return [('mtl-downtown', _('Montreal - Downtown')),
-            ('mtl-westisland', _('Montreal - West-Island'))  #'Montreal - Ouest de l''ile
+    return [('mtl-downtown', _(u'Montreal - Downtown').decode("UTF-8")),
+            ('mtl-westisland', _(u'Montreal - West-Island').decode("UTF-8"))
             ]
     
 # key, value
 def getAllCategories():
-    return [("physiotherapy", "Physiotherapeute"),
-            ("chiropractor", "Chiropracticien"),
-            ("osteopath", "Osteopathe")
+    return [(u"physiotherapy", _(u"Physiotherapist").decode("UTF-8")),
+            (u"chiropractor", _(u"Chiropractor").decode("UTF-8")),
+            (u"osteopath", _(u"Osteopath").decode("UTF-8"))
         ]
 
 # key, value
 def getAllSpecialities():
-    return [("cardiology", "Cardiologie"),
-            ("pneumology", "Pneumologie"),
-            ("orthopedic", "Orthop&eacute;die"),
-            ("sports", "Sportive"),
-            ("neurology", "Neurologie"),
-            ("pediatric", "P&eacute;diatrie"),
-            ("geriatric", "G&eacute;riatrie")
+    return [("cardiology", _(u"Cardiology").decode("UTF-8")),
+            ("pneumology", _(u"Pneumology").decode("UTF-8")),
+            ("orthopedic", _(u"Orthopedic").decode("UTF-8")),
+            ("sports", _(u"Sports").decode("UTF-8")),
+            ("neurology", _(u"Neurology").decode("UTF-8")),
+            ("pediatric", _(u"Pediatric").decode("UTF-8")),
+            ("geriatric", _(u"Geriatric").decode("UTF-8"))
         ]
 
 def getAllSchools():
-    return [("concordia", "Concordia University"),
-            ("mcgill", "McGill University"),
-            ("udem", "Universit&eacute; de Montr&eacute;al"),
-            ("uqtr", "Universit&eacute; de Qu&ebec &agrave; Trois-Rivieres"),
-            ("usherb", "Universit&eacute; de Sherbrooke")
+    return [("concordia", _(u"Concordia University").decode("UTF-8")),
+            ("mcgill", _(u"McGill University").decode("UTF-8")),
+            ("udem", _(u"Université de Montréal").decode("UTF-8")),
+            ("uqtr", _(u"Université de Québec à Trois-Rivières").decode("UTF-8")),
+            ("usherb", _(u"Université de Sherbrooke").decode("UTF-8"))
         ]
 
 def getAllDiplomas():
-    return [("bachelors", "Baccalaureat"),
-            ("masters", "Maitrise"),
-            ("doctor", "Doctorat")
+    return [("bachelors", _(u"Bachelor's").decode("UTF-8")),
+            ("masters", _(u"Master's").decode("UTF-8")),
+            ("phd", _(u"Ph.D").decode("UTF-8"))
         ]
 
 
@@ -53,21 +55,22 @@ def getDatesList():
     oneDay = timedelta(days=1)
     for n in range(21):
         d = d + oneDay
-        dateTuple = (str(d), formatDateFR(d))
+        dateTuple = (unicode(d), formatDateFR(d))
         datesList.append(dateTuple)
     return datesList
 
 def getTimesList():
-    startTimeList = range(7,22)
-    timeStringList = map(lambda x: (str(x), formatTimeFR(x)), startTimeList)
+    startTimeList = range(7, 22)
+    timeStringList = map(lambda x: (unicode(x), formatTimeFR(x)), startTimeList)
     return timeStringList
 
 def formatDateFR(date):
-    return format_date(date, "d MMMM yyyy (EEEE)", locale='fr')
+    return format_date(date, u"d MMMM yyyy (EEEE)", locale='fr')
 
 def formatTimeFR(startTime):
-    return str(startTime) + 'h - ' + str(startTime+1) + 'h'
+    return unicode(startTime) + 'h - ' + str(startTime + 1) + 'h'
 
+# is this method used anywhere?
 def formatDateTimeNoSeconds(datetime):
-    return format_datetime(datetime, "d MMMM yyyy (EEEE) H:m", locale='en')
+    return format_datetime(datetime, u"d MMMM yyyy (EEEE) H:m", locale='en')
 
