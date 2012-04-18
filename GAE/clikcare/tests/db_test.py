abde794218f0134f609b1e0dea9d4781e1e3fb48
phiiil <phiiil3737@gmail.com>
2012-04-18 10:29:46 -0400

    Added test for findBestProvider

--- GAE/clikcare/tests/db_test.py
+++ GAE/clikcare/tests/db_test.py
@@ -2,9 +2,14 @@ import unittest
 from google.appengine.ext import db as gdb
 from google.appengine.ext import testbed
 import db
+import logging, sys
+from data import Provider, Booking
 
-class DemoTestCase(unittest.TestCase):
+class DBTestCase(unittest.TestCase):
     def setUp(self):
+        logging.basicConfig(level=logging.DEBUG)
+        logging.info("TEST LOGGING")
+        print('setup')
         # First, create an instance of the Testbed class.
         self.testbed = testbed.Testbed()
         # Then activate the testbed, which prepares the service stubs for use.
@@ -13,12 +18,30 @@ class DemoTestCase(unittest.TestCase):
         self.testbed.init_datastore_v3_stub()
 
     def tearDown(self):
+        print('tearDown')
         self.testbed.deactivate()
 
     def testStoreBooking(self):
         values = { 'bookingCategory': 'physiotherapy',
                    'bookingRegion': 'mtl-downtown' 
                  }
-        key = db.storeBooking(values)
+        key = db.storeBooking(values, None, None)
         print key
         
+    def testFindBestProviderForBooking(self):
+        testCategory = u'physiotherapy'
+        # create provider
+        p = Provider()
+        p.firstName = 'Best-Test'
+        p.lastName = 'Phys-Io'
+        p.category = testCategory
+        pkey = p.put()
+        # create booking
+        b = Booking()
+        b.requestCategory = testCategory
+        b.put();
+        # test the matching
+        bestProvider = db.findBestProviderForBooking(b)
+        logging.info('best provider:' + str(bestProvider))
+        # assert
+        self.assertEqual(pkey, bestProvider.key(), 'provider keys should be equal')
