9ec0db70c9c3d6d14e0af555a88be69910ec6564
David L <leblancd@gmail.com>
2012-04-26 00:47:17 -0400

    Removed login code from admin handler, now in generic login handler and called from app.yaml

--- GAE/clikcare/admin.py
+++ GAE/clikcare/admin.py
@@ -28,18 +28,8 @@ class AdminIndexHandler(AdminBaseHandler):
 class AdminBookingsHandler(AdminBaseHandler):
     '''Administer Bookings'''
     def get(self):
-        user = users.get_current_user()
-        if user:
-            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" % 
-                        (user.nickname(), users.create_logout_url("/")))
-            logging.info(greeting)
             bookings = gdb.GqlQuery("SELECT * FROM Booking ORDER BY createdOn DESC LIMIT 10")
             self.render_template('admin/admin_bookings.html', bookings=bookings)
-
-        else:
-            greeting = ("<a href=\"%s\">Sign in or register</a>." % 
-                        users.create_login_url("/"))
-            self.response.out.write("<html><body>%s</body></html>" % greeting)
 
 
 class AdminProvidersHandler(AdminBaseHandler):
--- GAE/clikcare/login.py
+++ GAE/clikcare/login.py
@@ -6,9 +6,11 @@ class LoginHandler(webapp.RequestHandler):
     def get(self):
         logging.info("LoginHandler Invoked")
         user = users.get_current_user()
+        
         if user:
             greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
                         (user.nickname(), users.create_logout_url("/")))
+            logging.info(greeting)
         else:
             greeting = ("<a href=\"%s\">Sign in or register</a>." %
                         users.create_login_url("/"))
