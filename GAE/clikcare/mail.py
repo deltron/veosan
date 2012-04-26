9e47b190a0b3f5cdb0f2e1723528ce85382e21a4
phiiil <phiiil3737@gmail.com>
2012-04-26 12:30:54 -0400

    Added more info in the email

--- GAE/clikcare/mail.py
+++ GAE/clikcare/mail.py
@@ -1,12 +1,17 @@
 
 import logging
+from babel import Locale
+from base import lang
 from google.appengine.api import mail
+import datetime
 
 CLIK_SUPPORT_ADDRESS = 'philippe.caya@gmail.com'
 
 def emailBooking(booking):
     ''' send booking info to patient, provider and us '''
     patient = booking.patient
+    provider = booking.provider
+    category = provider.category
     to_address = patient.email
     # check email valid
     if not mail.is_email_valid(to_address):
@@ -15,10 +20,14 @@ def emailBooking(booking):
     message = mail.EmailMessage()
     message.sender = CLIK_SUPPORT_ADDRESS
     message.to = to_address
-    message.body = """
-Your are booked!
+    message.subject = u'Cliksoin Reservation - %s' % _(category).decode("UTF-8").capitalize()
+    message.body = '''
+Your are booked for {0} with {1} on {2}
 
-%s
-    """ % booking.__dict__
+{3}
+    '''.format(_(category), provider.fullName(), booking.dateTime, booking.__dict__)
     # send message
     message.send()
+    
+    
+    #{:%Y-%m-%d %H:%M}
