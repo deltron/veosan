936df7c6429681af6753ce2765c4ba3ff1abeef5
David L <leblancd@gmail.com>
2012-05-24 22:24:49 -0400

    added test for double booking

--- GAE/tests/integration/booking_test.py
+++ GAE/tests/integration/booking_test.py
@@ -92,8 +92,85 @@ class BookingTest(AdminTest):
         response.mustcontain("We currently do not have a health-care professional available that matches your needs and schedule.")
         response.mustcontain("Please fill in your email below and we will contact you as soon as we have availability.")
 
- 
+    def test_booking_twice_in_same_timeslot(self):
+        ''' Create a booking in the timeslot after another booking is made in the same timeslot '''
+        
+        AdminTest.test_complete_profile_creation(self)
+        
+        # at this point there is one fully completed profile with a single timeslot available (Monday 8-13)
+        
+        # go back to the main page and try to book monday 8am
+        response = self.testapp.post('/')
+                
+        # fill out the form
+        booking_form = response.forms[0] # booking form
+        booking_form['category'] = 'osteopath' # admin test created an osteopath
+        
+        booking_date_select = booking_form.fields['booking_date'][0]
+        
+        # find the option value with a monday
 
+        for (date_string, selected) in booking_date_select.options:
+            d = datetime.strptime(date_string, "%Y-%m-%d")
+            if d.weekday() == 0: # choose the first monday on the form
+                booking_form['booking_date'] = date_string
+                break
+        
+        # leave time to default (should be 8-9h)
+
+        # leave region to default (should be downtown)
+        
+        response = booking_form.submit()
+        
+        # verify provider name
+        response.mustcontain("Mr. Fantastic F.", "is available")
+        
+        # verify location
+        response.mustcontain("at his clinic on 123 Main St. in Westmount")
+        
+        # verify date and time
+        response.mustcontain("8:00")
+    
+        d = datetime.strptime(date_string + " 8:00", "%Y-%m-%d %H:%M")
+        formatted_date = util.format_datetime_full(d)
+        response.mustcontain(formatted_date)
+    
+        # verify bio and quote
+        response.mustcontain("The quick brown fox jumped over the lazy dog")
+        response.mustcontain("Areas of interest include treatment and management of spinal conditions with an emphasis on manual therapy and rehabilitative exercise.")
+        
+        #### 
+        # now try to book again
+
+        # go back to the main page and try to book monday 8am
+        response = self.testapp.post('/')
+                
+        # fill out the form
+        booking_form = response.forms[0] # booking form
+        booking_form['category'] = 'osteopath' # admin test created an osteopath
+        
+        booking_date_select = booking_form.fields['booking_date'][0]
+        
+        # find the option value with a monday
+
+        for (date_string, selected) in booking_date_select.options:
+            d = datetime.strptime(date_string, "%Y-%m-%d")
+            if d.weekday() == 0: # choose the first monday on the form
+                booking_form['booking_date'] = date_string
+                break
+        
+        # leave time to default (should be 8-9h)
+
+        # leave region to default (should be downtown)
+        
+        response = booking_form.submit()
+        
+        # verify error messages
+        response.mustcontain("Fully Booked!")
+        response.mustcontain("We currently do not have a health-care professional available that matches your needs and schedule.")
+        response.mustcontain("Please fill in your email below and we will contact you as soon as we have availability.")
+       
+        
 if __name__ == "__main__":
     unittest.main()
     
