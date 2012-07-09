4eeeaacec6d01dd918af8d0cfdff99b85140bbe5
David L <leblancd@gmail.com>
2012-07-09 09:34:37 -0400

    Fixed error message when there is a problem sending comments

--- GAE/veosan/handler/contact.py
+++ GAE/veosan/handler/contact.py
@@ -18,7 +18,7 @@ class ContactHandler(BaseHandler):
                 mail.email_contact_form(self.jinja2, contact_form.from_email.data, contact_form.subject.data, contact_form.message_body.data)
                 self.render_template('contact.html', form=contact_form, success_message=_(u"Your comments were sent succesfully. Thanks for your feedback, we will contact you shortly to follow up. Have a great day!"))
             except Exception as e:
-                self.render_template('contact.html', error_message=_(u"Sorry, there was an error sending your comments. Please call 555-123-1212 to talk to someone."))
+                self.render_template('contact.html', error_message=_(u"Sorry, there was an error sending your comments. Please call (514) 998-1286 to talk with someone."))
         else:
             self.render_template('contact.html', form=contact_form)
             
