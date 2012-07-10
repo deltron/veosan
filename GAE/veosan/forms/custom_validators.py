32bd82b176ba3ae601adad85c147b247ceaf67b6
David L <leblancd@gmail.com>
2012-07-10 10:54:56 -0400

    New custom validators: - Unique Email addres validation - No whitespace check

--- GAE/veosan/forms/custom_validators.py
+++ GAE/veosan/forms/custom_validators.py
@@ -20,3 +20,30 @@ class UniqueVanityURL(object):
                 #available
                 pass
 
+class UniqueEmail(object):
+    def __init__(self, message=None):
+        self.message = message
+
+    def __call__(self, form, field):
+        email = field.data
+    
+        if email:
+            # try to fetch a provider with this email address
+            provider = db.get_provider_from_email(email)
+            
+            if provider:
+                # taken
+                raise ValidationError(self.message)
+    
+            else:
+                #available
+                pass
+
+
+class NoWhitespace(object):
+    def __init__(self, message=None):
+        self.message = message
+
+    def __call__(self, form, field):
+        if ' ' in field.data:
+            raise ValidationError(self.message)
