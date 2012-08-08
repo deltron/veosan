1f830e6705a609e89a304429d5d63e496ad9ca0e
David L <leblancd@gmail.com>
2012-08-08 11:31:39 -0400

    - implemented delete test - implemented test for no modal when on default page

--- GAE/tests/integration/provider_schedule_test.py
+++ GAE/tests/integration/provider_schedule_test.py
@@ -6,13 +6,23 @@ from data import db
 
 class ProviderTest(BaseTest):
     
+    def test_no_form_on_schedule_page(self):
+        self.self_signup_provider(self._TEST_PROVIDER_EMAIL, self._TEST_PROVIDER_VANITY_URL)
+
+        response = self.testapp.get('/provider/schedule/' + self._TEST_PROVIDER_VANITY_URL)
+        
+        response.mustcontain(no="schedule_form")
+        
+        
     def test_add_time_to_schedule(self):
         self.self_signup_provider(self._TEST_PROVIDER_EMAIL, self._TEST_PROVIDER_VANITY_URL)
 
         response = self.testapp.get('/provider/schedule/' + self._TEST_PROVIDER_VANITY_URL)
 
+        # click on a button
+        response = self.testapp.get('/provider/schedule/' + self._TEST_PROVIDER_VANITY_URL + '/add/tuesday/9')
+
         schedule_form = response.forms['schedule_form']
-        
         schedule_form['day'] = 'tuesday'
         schedule_form['start_time'] = 9
         schedule_form['end_time'] = 17
@@ -22,8 +32,31 @@ class ProviderTest(BaseTest):
         # check on the schedule admin page
         response.mustcontain('9 AM-5 PM')
 
+
     def test_delete_time_from_schedule(self):
-        pass
+        self.self_signup_provider(self._TEST_PROVIDER_EMAIL, self._TEST_PROVIDER_VANITY_URL)
+
+        response = self.testapp.get('/provider/schedule/' + self._TEST_PROVIDER_VANITY_URL)
+
+        # click on a button
+        response = self.testapp.get('/provider/schedule/' + self._TEST_PROVIDER_VANITY_URL + '/add/tuesday/9')
+
+        schedule_form = response.forms['schedule_form']
+        schedule_form['day'] = 'tuesday'
+        schedule_form['start_time'] = 9
+        schedule_form['end_time'] = 17
+
+        response = schedule_form.submit()
+        
+        # check on the schedule admin page
+        response.mustcontain('9 AM-5 PM')
+
+        # now find and delete it
+        modal_response = response.click(linkid='tuesday_9_link')
+
+        delete_response = modal_response.click(description=' Delete ')
+        
+        delete_response.mustcontain(no='9 AM-5 PM')
     
     def test_change_time_in_schedule(self):
         pass
