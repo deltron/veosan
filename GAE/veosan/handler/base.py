a7b0e5b02728b80ee235eec9009cb25d66564d5e
David L <leblancd@gmail.com>
2012-08-22 16:41:40 -0400

    handle situation where there is NO user_agent (ie. browser ID). This happens in unit tests

--- GAE/veosan/handler/base.py
+++ GAE/veosan/handler/base.py
@@ -136,27 +136,31 @@ class BaseHandler(webapp2.RequestHandler):
         # render
                 
         # check if we have internet exploder
-        browser = self.request.headers.get('User-Agent')
-        is_msie = re.search("MSIE ([0-9]{1,}[\.0-9]{0,})", browser);
-        logging.info("Browser User-Agent: %s" % browser)
-
-        if is_msie:
-            msie_str = is_msie.group()
-            version_str = re.search("([0-9]{1,}[\.0-9]{0,})", msie_str)
-
-            if version_str:
-                version = float(version_str.group())
-                if version < 9:
-                    self.response.write(self.jinja2.render_template('internet_explorer.html', **kw))
-                    site_counter = db.get_site_counter()
-                    site_counter.internet_explorer_hits += 1
-                    site_counter.put_async()
+        user_agent = self.request.headers.get('User-Agent')
+        if user_agent:
+            is_msie = re.search("MSIE ([0-9]{1,}[\.0-9]{0,})", user_agent);
+            logging.info("Browser User-Agent: %s" % user_agent)
+    
+            if is_msie:
+                msie_str = is_msie.group()
+                version_str = re.search("([0-9]{1,}[\.0-9]{0,})", msie_str)
+    
+                if version_str:
+                    version = float(version_str.group())
+                    if version < 9:
+                        self.response.write(self.jinja2.render_template('internet_explorer.html', **kw))
+                        site_counter = db.get_site_counter()
+                        site_counter.internet_explorer_hits += 1
+                        site_counter.put_async()
+                    else:
+                        self.response.write(self.jinja2.render_template(filename, **kw))
                 else:
-                    self.response.write(self.jinja2.render_template(filename, **kw))
+                    logging.error("Unable to parse version string for Internet Explorer: %s" % is_msie.group())
+    
             else:
-                logging.error("Unable to parse version string for Internet Explorer: %s" % is_msie.group())
-
+                self.response.write(self.jinja2.render_template(filename, **kw))
         else:
+            logging.error("Unable to parse user agent")
             self.response.write(self.jinja2.render_template(filename, **kw))
         
         # log request in database
