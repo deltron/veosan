5f908d67ce43d9b0b909c6046a9bdd963d687387
phiiil <phiiil3737@gmail.com>
2012-08-24 16:25:07 -0400

    Added test for booking from returning non-registered patient (a la OpenTable)

--- GAE/tests/integration/public_profile_test.py
+++ GAE/tests/integration/public_profile_test.py
@@ -150,79 +150,51 @@ class PublicProfileTest(BaseTest):
         
         
     def test_book_from_public_profile_new_patient(self):
+        self.create_provider_and_enable_booking()
+        # Book from public profile
         date_string = testutil.next_monday_date_string()
         time_string = '10'
         self.book_from_public_profile(date_string, time_string)
-
-        # check provider bookings list, should be empty as booking is not confirmed  
-        # switch to french
-        repsonse = self.testapp.get('/lang/fr')
-
+        
+        # provider should not see the booking yet, check provider bookings list, should be empty as booking is not confirmed
         self.login_as_provider()
         provider_bookings = self.testapp.get('/provider/bookings/' + self._TEST_PROVIDER_VANITY_URL)
         provider_bookings.mustcontain('Vous n’avez aucun rendez-vous prévu')
-        self.logout_provider()
-        
-        # check patient's booking list
-        #self.login_as_patient()
-        
-        # check datetime
-        #self.logout_as_patient()
-        
-
-        
-        # check event logs
-        
-        
-        booking_datetime = datetime.strptime(testutil.next_monday_date_string() + " 10", '%Y-%m-%d %H')
-        french_datetime_string = format_datetime(booking_datetime, "EEEE 'le' d MMMM yyyy", locale='fr_CA') + " à " + format_datetime(booking_datetime, "H:mm", locale='fr_CA')
-        logging.info('French date time of booking: %s' % french_datetime_string) 
         # no email sent to provider (patient is not confirmed)
         messages = self.mail_stub.get_sent_messages(to=self._TEST_PROVIDER_EMAIL)
         self.assertEqual(0, len(messages))
-        # check that confirmation emails was sent to patient
-        messages = self.mail_stub.get_sent_messages(to=self._TEST_PATIENT_EMAIL)
-        self.assertEqual(1, len(messages))
-        m = messages[0]
-        self.assertEqual(self._TEST_PATIENT_EMAIL, m.to)
+        self.logout_provider()
         
-        # activate account
-        messages = self.mail_stub.get_sent_messages(to=self._TEST_PATIENT_EMAIL)
-        
-        self.assertEquals(m.subject, 'Rendez-vous Veosan - Ostéopathe')
-        #self.assertEqual(m.sender, 'first last <support@veosan.com>')
-        #self.assertEqual(m.reply_to, self._TEST_PROVIDER_EMAIL)
-        #self.assertIn('Please click on the link below to create your profile', m.body.payload)
-        #self.assertIn("I've been using Veosan and thought you might like to try it out. Here's an invitation to create a profile.", m.body.payload)
-        user = db.get_user_from_email(self._TEST_PATIENT_EMAIL)
-        # check email content
-        self.assertTrue('/user/activation/%s' % user.signup_token in m.body.payload)
-        self.assertIn('Bonjour', m.body.payload)
-        self.assertIn('Merci', m.body.payload)
-        # click the link
-        confirmation_page = self.testapp.get('/user/activation/%s' % user.signup_token)
-        confirmation_page.mustcontain('Votre rendez-vous est confirmé')
-        confirmation_page.mustcontain(french_datetime_string)
-        confirmation_page.mustcontain("Fantastic Fox")
-        # Check email to provider    
-        messages = self.mail_stub.get_sent_messages(to=self._TEST_PROVIDER_EMAIL)
-        self.assertEqual(1, len(messages))
-        provider_mail = messages[0]
-        self.assertEquals(provider_mail.subject, 'Veosan - Nouveau rendez-vous avec Pat Patient')
-        #self.assertEqual(m.sender, 'first last <support@veosan.com>')
-        #self.assertEqual(m.reply_to, self._TEST_PROVIDER_EMAIL)
-        self.assertIn('Vous avez un nouveau rendez-vous', provider_mail.body.payload)
-        self.assertIn("Consultez vos rendez-vous ici", provider_mail.body.payload)
+        # patient confirms
+        self.patient_confirms_latest_booking(date_string, time_string)
 
+    def test_public_profile_book_no_password_returning_patient(self):
+        self.create_provider_and_enable_booking()
+        # book once from public profile
+        date_string = testutil.next_monday_date_string()
+        time_string = '10'
+        self.book_from_public_profile(date_string, time_string)
         
-        # check status change in all lists (provider, patient and admin dashboards)
-       
+        # provider should not see the booking yet, check provider bookings list, should be empty as booking is not confirmed
         self.login_as_provider()
         provider_bookings = self.testapp.get('/provider/bookings/' + self._TEST_PROVIDER_VANITY_URL)
-        provider_bookings.mustcontain('Pat Patient')
-        # check datetime
-        provider_bookings.mustcontain(french_datetime_string)
+        provider_bookings.mustcontain('Vous n’avez aucun rendez-vous prévu')
+        # no email sent to provider (patient is not confirmed)
+        messages = self.mail_stub.get_sent_messages(to=self._TEST_PROVIDER_EMAIL)
+        self.assertEqual(0, len(messages))
         self.logout_provider()
+        
+        self.patient_confirms_latest_booking(date_string, time_string)
+        
+        # book second time from public profile
+        date_string = testutil.next_monday_date_string()
+        time_string = '11'
+        self.book_from_public_profile(date_string, time_string, returning_patient=True)
+        self.patient_confirms_latest_booking(date_string, time_string)
+        
+        self.login_as_provider()
+        provider_bookings = self.testapp.get('/provider/bookings/' + self._TEST_PROVIDER_VANITY_URL)
+        provider_bookings.showbrowser()
         
 
 if __name__ == "__main__":
