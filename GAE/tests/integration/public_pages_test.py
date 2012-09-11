fb0a010b46aea1d52d24db15a63e84e19394cabd
phiiil <phiiil3737@gmail.com>
2012-09-11 10:08:34 -0400

    Translated index links Added tests for index language switch

--- GAE/tests/integration/public_pages_test.py
+++ GAE/tests/integration/public_pages_test.py
@@ -11,17 +11,19 @@ class ProviderSocialTest(BaseTest):
         index_page.mustcontain(u'Pour les professionnels de la santé')
         index_page.mustcontain('Blogue')
         
-        
     def test_english_index(self):
         en_index_page = self.testapp.get('/en')
         en_index_page.mustcontain('For Health Care Professionals')
         en_index_page.mustcontain('Français')
+        fr_index_page = en_index_page.click(linkid='lang-change-link')
+        fr_index_page.mustcontain('Pour les professionnels de la santé')
         
     def test_french_index(self):
-        en_index_page = self.testapp.get('/fr')
-        en_index_page.mustcontain('Pour les professionnels de la santé')
-        en_index_page.mustcontain('English')
-        
+        fr_index_page = self.testapp.get('/fr')
+        fr_index_page.mustcontain('Pour les professionnels de la santé')
+        fr_index_page.mustcontain('English')
+        en_index_page = fr_index_page.click(linkid='lang-change-link')
+        en_index_page.mustcontain('For Health Care Professionals')
         
     def test_tour_page(self):
         # Francais
@@ -30,9 +32,10 @@ class ProviderSocialTest(BaseTest):
         # switch to English
         en_tour_page = fr_tour_page.click(linkid='lang-change-link')
         en_tour_page.mustcontain('Help', 'patients', 'find you')
-        
         # english
         en_tour_page = self.testapp.get('/en/tour')
         en_tour_page.mustcontain('Help', 'patients', 'find you')
-        # switch to Franch
+        # switch to French
+        fr_tour_page = en_tour_page.click(linkid='lang-change-link')
+        fr_tour_page.mustcontain('Aidez vos', 'patients', 'à vous trouver')
         
