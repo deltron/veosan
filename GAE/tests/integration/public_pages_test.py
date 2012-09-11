7215877ade8a075d33e607709425e97736abe525
phiiil <phiiil3737@gmail.com>
2012-09-11 10:55:33 -0400

    Added test for login page and language change

--- GAE/tests/integration/public_pages_test.py
+++ GAE/tests/integration/public_pages_test.py
@@ -4,7 +4,7 @@ from base import BaseTest
 import unittest
 from data import db
 
-class ProviderSocialTest(BaseTest):
+class PublicPagesTest(BaseTest):
     
     def test_default_language(self):
         index_page = self.testapp.get('/')
@@ -38,4 +38,23 @@ class ProviderSocialTest(BaseTest):
         # switch to French
         fr_tour_page = en_tour_page.click(linkid='lang-change-link')
         fr_tour_page.mustcontain('Aidez vos', 'patients', 'à vous trouver')
+        
+    def test_login_page(self):
+        # from index
+        index_page = self.testapp.get('/fr')
+        fr_page = index_page.click(linkid='login-link')
+        fr_page.mustcontain('Connexion à Veosan')
+        # Direct link Francais
+        fr_page = self.testapp.get('/fr/login')
+        fr_page.mustcontain('Connexion à Veosan')
+        # switch to English
+        en_page = fr_page.click(linkid='lang-change-link')
+        en_page.mustcontain('Login to Veosan')
+        # direct link english
+        en_page = self.testapp.get('/en/login')
+        en_page.mustcontain('Login to Veosan')
+        # switch to French
+        fr_page = en_page.click(linkid='lang-change-link')
+        fr_page.mustcontain('Connexion à Veosan')
+        
         
