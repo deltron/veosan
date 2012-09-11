ae556a10083ab337319ccdd69eca4aee72a6480c
David L <leblancd@gmail.com>
2012-09-11 13:05:39 -0400

    added language prefix to routes for signup

--- GAE/veosan/routes/user_routes.py
+++ GAE/veosan/routes/user_routes.py
@@ -6,13 +6,15 @@ from handler import user
 
 
 def get_routes():
-    return [ PathPrefixRoute('/signup', [Route('/patient', signup_handler.PatientSignupHandler),
+    return [ PathPrefixRoute('/en/signup', [Route('/patient', signup_handler.PatientSignupHandler),
                                             Route('/provider', signup_handler.ProviderSignupHandler1),
                                             Route('/provider2', signup_handler.ProviderSignupHandler2),
-                                            Route('/patient/<lang_key>', signup_handler.PatientSignupHandler),
-                                            Route('/provider/<lang_key>', signup_handler.ProviderSignupHandler1),
                ]),
-        
+             PathPrefixRoute('/fr/signup', [Route('/patient', signup_handler.PatientSignupHandler),
+                                            Route('/provider', signup_handler.ProviderSignupHandler1),
+                                            Route('/provider2', signup_handler.ProviderSignupHandler2),
+               ]),
+               
                Route('/login', user.LoginHandler),
                Route('/en/login', user.LoginHandler, handler_method='get_en'),
                Route('/fr/login', user.LoginHandler, handler_method='get_fr'),
