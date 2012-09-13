8b298531c0aef233d580f487b3cb495ae40e17ca
David L <leblancd@gmail.com>
2012-09-13 14:29:52 -0400

    added test for signup from prospect

--- GAE/tests/integration/prospect_test.py
+++ GAE/tests/integration/prospect_test.py
@@ -39,6 +39,8 @@ class ProspectTest(BaseTest):
         response.mustcontain("It's Who You Are!")
         
         # click on signup page
+        response.mustcontain("/en/signup/provider")
+
         response = self.testapp.get('/en/signup/provider')
 
         # log in as admin and check the logs
@@ -110,6 +112,29 @@ class ProspectTest(BaseTest):
         response = prospect_form.submit()
                 
         response.mustcontain("Prospect ID is not unique")
+
+
+    def test_add_prospect_signup_page_complete_process(self):
+        self.create_prospect()
+        
+        # hit up the prospect signup url
+        response = self.testapp.get('/signup/103')
+        
+        # should be the 2nd signup page (prepopulated)
+        response.mustcontain("You are just one click away from having your own online presence.")
+        response.mustcontain('<input id="first_name" name="first_name" type="hidden" value="%s">' % 'Al')
+        response.mustcontain('<input id="last_name" name="last_name" type="hidden" value="%s">' % 'Swearingen')
+        response.mustcontain('<option selected value="%s">' % 'doctor')
+        
+        signup_form = response.forms['provider_signup_form2']
+        signup_form['password'] = self._TEST_PROVIDER_PASSWORD
+        signup_form['password_confirm'] = self._TEST_PROVIDER_PASSWORD
+        
+        welcome_response = signup_form.submit().follow()
+        
+        # should be on the welcome page
+        welcome_response.mustcontain("Welcome!")
+
 
 if __name__ == "__main__":
     unittest.main()
