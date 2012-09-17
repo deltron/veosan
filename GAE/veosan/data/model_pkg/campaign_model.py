de568a8a21519039a7659bec48ff5c420a41ce71
phiiil <phiiil3737@gmail.com>
2012-09-17 16:19:15 -0400

    First stab at generation emails for campaign

--- GAE/veosan/data/model_pkg/campaign_model.py
+++ GAE/veosan/data/model_pkg/campaign_model.py
@@ -1,17 +1,17 @@
 from google.appengine.ext import ndb
 
 class EmailCampaign(ndb.Model):
-    
+    # creation date
     created_on = ndb.DateTimeProperty(auto_now_add=True)
-    
     # unique name this prospect
     name = ndb.StringProperty()
-    
+    description = ndb.StringProperty()
+    # Email Template - Francais
     subject_fr = ndb.StringProperty()
     body_fr = ndb.TextProperty()
-    
+    # Email Temaplte - English
     subject_en = ndb.StringProperty()
     body_en = ndb.TextProperty()
-    
+    # list of prospects
     prospects = ndb.KeyProperty(repeated=True)
     
