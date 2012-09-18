44f244c6de884ac1b0a295c99f503ebc1da9848e
David L <leblancd@gmail.com>
2012-09-18 16:27:07 -0400

    - added more metadata to prospect (last site visit, number and type of notes, last note entry) - removed delete button for prospect

--- GAE/veosan/data/model_pkg/prospect_model.py
+++ GAE/veosan/data/model_pkg/prospect_model.py
@@ -14,6 +14,8 @@ class ProspectNote(ndb.Model):
 
 
 class ProviderProspect(ndb.Model):
+    created_on = ndb.DateTimeProperty(auto_now_add=True)
+
     # address from AppEngine
     gae_country = ndb.StringProperty()
     gae_region = ndb.StringProperty()
@@ -33,6 +35,7 @@ class ProviderProspect(ndb.Model):
     # prospect status
     status = ndb.StringProperty(default='new', choices=util.prospect_statuses)
 
+
     # eventually link to a provider if they sign up
     #provider = ndb.KeyProperty(kind='Provider')
     
@@ -40,9 +43,38 @@ class ProviderProspect(ndb.Model):
     def get_site_logs(self):
         return SiteLog.query(SiteLog.prospect == self.key).order(-SiteLog.access_time).fetch()
     
+    def get_last_site_visit_timestamp(self):
+        latest_site_visit = SiteLog.query(SiteLog.prospect == self.key).order(-SiteLog.access_time).get()
+        if latest_site_visit:
+            return latest_site_visit.access_time
+        else:
+            return None
+
     def get_notes(self):
         return ProspectNote.query(ProspectNote.prospect == self.key).order(-ProspectNote.created_on).fetch()
-    
+
+    def get_last_note_timestamp(self):
+        latest_prospect_note = ProspectNote.query(ProspectNote.prospect == self.key).order(-ProspectNote.created_on).get()
+        if latest_prospect_note:
+            return latest_prospect_note.created_on
+        else:
+            return None
+
+    def get_notes_count(self):
+        return ProspectNote.query(ProspectNote.prospect == self.key).count()
+
+    def get_notes_info_count(self):
+        return ProspectNote.query(ProspectNote.prospect == self.key, ProspectNote.note_type == 'info').count()
+
+    def get_notes_meeting_count(self):
+        return ProspectNote.query(ProspectNote.prospect == self.key, ProspectNote.note_type == 'meeting').count()
+
+    def get_notes_email_count(self):
+        return ProspectNote.query(ProspectNote.prospect == self.key, ProspectNote.note_type == 'email').count()
+
+    def get_notes_call_count(self):
+        return ProspectNote.query(ProspectNote.prospect == self.key, ProspectNote.note_type == 'call').count()
+
     def get_blog_url(self):
         # Replace HOST for dev
         return 'http://veosan.com/blog/%s' % self.prospect_id
