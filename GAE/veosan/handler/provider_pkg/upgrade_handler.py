4abf774c792845eded39e9499b9be94fd33e0aa4
David L <leblancd@gmail.com>
2012-10-12 14:54:46 -0400

    detect server address and switch from production to test key depending on deployed location

--- GAE/veosan/handler/provider_pkg/upgrade_handler.py
+++ GAE/veosan/handler/provider_pkg/upgrade_handler.py
@@ -6,16 +6,26 @@ from webapp2_extras.i18n import lazy_gettext as _
 import stripe
 from data.model_pkg.provider_model import ProviderAccount
 from stripe import CardError
+import util
 
-_STRIPE_TEST_KEY = "sk_0CGm5DhYaiuarjtM4adlmooPeclET"
-_STRIPE_PROD_KEY = "sk_0CGm0CBZ8jbZX5N1W1hJ9vH36tDff"
+_STRIPE_TEST_KEY_SECRET = "sk_0CGm5DhYaiuarjtM4adlmooPeclET"
+_STRIPE_PROD_KEY_SECRET = "sk_0CGm0CBZ8jbZX5N1W1hJ9vH36tDff"
+
+_STRIPE_TEST_KEY_PUBLIC = 'pk_0CGm7P3z0UJVLSfX3Pj5z7NKuzUjj'
+_STRIPE_PROD_KEY_PUBLIC = "pk_0CGmD1EM1Gz1vUdFrUl2WSP3Xep9a"
+
 
 class ProviderUpgradeHandler(ProviderBaseHandler):
     @provider_required
     def get(self, vanity_url=None):
         provider = db.get_provider_from_vanity_url(vanity_url)
+        stripe_key = _STRIPE_TEST_KEY_PUBLIC
+        if util.is_dev_server(self.request):
+            stripe_key = _STRIPE_TEST_KEY_PUBLIC
+        else:
+            stripe_key = _STRIPE_PROD_KEY_PUBLIC
 
-        self.render_template("provider/upgrade.html", provider=provider)
+        self.render_template("provider/upgrade.html", provider=provider, stripe_key=stripe_key)
 
     @provider_required
     def post(self, vanity_url=None):
@@ -23,7 +33,10 @@ class ProviderUpgradeHandler(ProviderBaseHandler):
 
         # set your secret key: remember to change this to your live secret key in production
         # see your keys here https://manage.stripe.com/account
-        stripe.api_key = _STRIPE_TEST_KEY
+        if util.is_dev_server(self.request):
+            stripe.api_key = _STRIPE_TEST_KEY_SECRET
+        else:
+            stripe.api_key = _STRIPE_PROD_KEY_SECRET
         
         # get the credit card details submitted by the form
         token = self.request.POST['stripeToken']
